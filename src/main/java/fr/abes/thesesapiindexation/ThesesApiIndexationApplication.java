ae4631f664d9b72ae44b2ee83fa65c6a6b194742
Jerome Villiseck <jvk@abes.fr>
2026-07-27 11:16:26 +0200

    build: créer le socle Maven de l'API d'indexation
    Ajoute le projet Spring Boot Java 17, son point d'entrée et les règles d'exclusion des fichiers générés.

---
+++ src/main/java/fr/abes/thesesapiindexation/ThesesApiIndexationApplication.java
@@ -0,0 +1,12 @@
+package fr.abes.thesesapiindexation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesesApiIndexationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThesesApiIndexationApplication.class, args);
+    }
+}
